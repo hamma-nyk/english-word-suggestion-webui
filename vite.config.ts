70796a186f28e4e63adaac44544237c9e3cdff95
hamma-nyk <yikonyk@gmail.com>
2025-11-28 21:30:03 +0700

    CEK

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  root: ".", // root project
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
